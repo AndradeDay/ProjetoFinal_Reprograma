d6ef8de302b331afc2a9a9579732da5888dbf83b
AndradeDay <dayzalvz@gmai.com>
2019-12-17 12:45:39 -0300

    Finalizando o projeto, criando  autentificação

--- src/controller/adocaoController.js
+++ src/controller/adocaoController.js
@@ -1,30 +1,104 @@
 const { connect } = require('../models/database')
 const { criancasCollection } = require('../models/criancasSchema')
-const {cadastrosCollection}  = require('../models/cadastroSchema')
-const jwt  = require('jsonwebtoken')
+const { cadastrosCollection } = require('../models/cadastroSchema')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
-
+const novoSchema = {}
 connect()
+
+const SEGREDO = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0lO48na+yjkDl/P53SmGmcVEel1kDXbzqCiPx7JUcfSHcUX+AXNk4HwoBBtzw0DEw/e1HEI2xOmLjk//VO7uwcg6xTKYTD3JYs9ZE2LN5tF3STeuxO624F6QQ3pmuoMiFP3a7jRmJzTOGa+HV3ERtftEirRVa7J9XC7zWYs9VywIDAQAB'
 
 // Rota para cadastrar usuarios e Hasherizar senha 
 
-const cadastro =  (request, response) => {
+const cadastroUser = (request, response) => {
     const criptografarSenha = bcrypt.hashSync(request.body.senha)
     request.body.senha = criptografarSenha
+    request.body.grupo = 'user'
     const novoCadastro = new cadastrosCollection(request.body)
+
 
     novoCadastro.save((error) => {
         if (error) {
-          return response.status(500).send(error)
+            return response.status(500).send(error)
         }
-    
+
         return response.status(201).send(novoCadastro)
-      })
+    })
+}
+
+
+// rota para cadastrar com grupo ADMIN
+
+
+const cadastroAdmin = (request, response) => {
+ const criptografarSenha = bcrypt.hashSync(request.body.senha)
+ request.body.senha = criptografarSenha
+ request.body.grupo = 'admin'
+ const novoCadastro = new cadastrosCollection(request.body)
+
+ novoCadastro.save((error) => {
+     if(error){
+         return response.status(500).send(error)
+     }
+     return response.status(201).send(novoCadastro)
+ })
+}
+
+
+const login = async (request, response) => {
+    const cadastroEncontrado = await cadastrosCollection.findOne({ email: request.body.email })
+
+    if (cadastroEncontrado) {
+        const senhaCorreta = bcrypt.compareSync(request.body.senha, cadastroEncontrado.senha)
+
+        if (senhaCorreta) {
+            const token = jwt.sign(
+                {
+                    grupo: cadastroEncontrado.grupo
+                },
+                SEGREDO,
+                { expiresIn: 6000 }
+            )
+
+            return response.status(200).send({ token })
+        }
+
+        return response.status(401).send('Senha incorreta.')
     }
-    
+
+    return response.status(404).send('Cadastro não encontrado.')
+
+
+}
 
 
 
+const mostrarCadastros = (request, response) => {
+    cadastrosCollection.find((error, cadastros) => {
+        if(error){
+            return response.status(500).send(error)
+        } else{
+            return response.status(200).send(cadastros)
+        }
+    }) 
+}
+
+const deletarUser = (request, response) => {
+    const idParams = request.params.id
+
+    cadastrosCollection.findByIdAndDelete(idParams, (error, cadastro) => {
+        if (error) {
+            return response.Status(500).send(error)
+        } if(cadastro) {
+            return response.status(200).send('Cadastro deletado!')
+        } else{
+            return response.senha(404).send('cadastro não encontrado')
+        }
+    })
+}
+
+
+// rota para adicionar Crinaças 
 
 
 const addCrianca = (request, response) => {
@@ -45,7 +119,8 @@ const addCrianca = (request, response) => {
 }
 
 
-const mostrarCriancas = (request, response) => {
+
+const mostrarCriancasAdmin = (request, response) => {
     criancasCollection.find((error, criancas) => {
         if (error) {
             return response.status(500).sendo(error)
@@ -54,6 +129,35 @@ const mostrarCriancas = (request, response) => {
         }
     })
 }
+
+
+
+const criancasUser = (criancasCollection) => {
+
+    novoSchema.nome = criancasCollection[0].nome,
+        novoSchema.apeliddo = criancasCollection[0].apelido,
+        novoSchema.personalidade = criancasCollection[0].personalidade,
+        novoSchema.hobbies = criancasCollection[0].hobbies
+
+    console.log(criancasCollection[0].nome)
+    return novoSchema
+}
+
+const mostrarCriancasUser = (request, response) => {
+
+
+    criancasCollection.find((error, novoSchema) => {
+
+        if (error) {
+            return response.status(500).send(error)
+        } else {
+            return response.status(200).send(criancasUser(novoSchema))
+        }
+
+    })
+}
+
+
 
 
 const deletar = (request, response) => {
@@ -91,8 +195,13 @@ const update = (request, response) => {
 
 module.exports = {
     addCrianca,
-    mostrarCriancas,
-    deletar, 
+    cadastroUser,
+    cadastroAdmin,
+    mostrarCriancasAdmin,
+    mostrarCriancasUser,
+    mostrarCadastros,
+    deletar,
+    deletarUser,
     update,
-    cadastro
+    login
 } 
--- src/routes/criancasRoutes.js
+++ src/routes/criancasRoutes.js
@@ -1,12 +1,80 @@
 const express = require('express')
 const router = express.Router()
-const controller = require('../controller/adocaoController')
+const controllerAdocao = require('../controller/adocaoController')
+const jwt = require('jsonwebtoken')
+//const bcrypt = require('bcryptjs')
+
+const SEGREDO = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC0lO48na+yjkDl/P53SmGmcVEel1kDXbzqCiPx7JUcfSHcUX+AXNk4HwoBBtzw0DEw/e1HEI2xOmLjk//VO7uwcg6xTKYTD3JYs9ZE2LN5tF3STeuxO624F6QQ3pmuoMiFP3a7jRmJzTOGa+HV3ERtftEirRVa7J9XC7zWYs9VywIDAQAB'
 
 
-router.post('/adicionar', controller.addCrianca);
-router.post('/cadastrar', controller.cadastro);
-router.get('/criancas', controller.mostrarCriancas);
-router.delete('/deletar/:id', controller.deletar);
-router.patch('/atualizar/:id', controller.update);
+const autenticar = (request, response, next) => {
+    const authHeader = request.get('authorization')
+    let autenticado = false
+  
+    if (!authHeader) {
+      return response.status(401).send('Você precisa fazer login!')
+    }
+
+    const token = authHeader.split(' ')[1]
+  
+    jwt.verify(token, SEGREDO, (error, decoded) => {
+      if (error) {
+        autenticado = false
+      } else {
+        if (decoded.grupo == 'user' || decoded.grupo == 'admin') {
+          autenticado = true
+        } else {
+          autenticado = false
+        }
+      }
+    })
+  
+    if (!autenticado) {
+      return response.status(403).send('Acesso negado.')
+    }
+  
+    next()
+  }
+  
+  
+  const autenticarAdmin = (request, response, next) => {
+    const authHeader = request.get('authorization')
+    let autenticado = false
+  
+    if (!authHeader) {
+      return response.status(401).send('Você precisa fazer login!')
+    }
+  
+    const token = authHeader.split(' ')[1]
+  
+    jwt.verify(token, SEGREDO, (error, decoded) => {
+      if (error) {
+        autenticado = false
+      } else {
+        if (decoded.grupo == 'admin') {
+          autenticado = true
+        } else {
+          autenticado = false
+        }
+      }
+    })
+  
+    if (!autenticado) {
+      return response.status(403).send('Acesso negado.')
+    }
+  
+    next()
+  }
+
+router.post('/adicionarCrianca', autenticarAdmin, controllerAdocao.addCrianca);
+router.post('/cadastrarUser', controllerAdocao.cadastroUser);
+router.post('/cadastrarAdmin', controllerAdocao.cadastroAdmin);
+router.post('/login', controllerAdocao.login);
+router.get('/criancas', autenticarAdmin, controllerAdocao.mostrarCriancasAdmin);
+router.get('/criancasUser',autenticar, controllerAdocao.mostrarCriancasUser);
+router.get('/cadastros', autenticarAdmin,controllerAdocao.mostrarCadastros);
+router.delete('/deletar/:id',autenticarAdmin, controllerAdocao.deletar);
+router.delete('deletarUser/:id', autenticarAdmin, controllerAdocao.deletarUser);
+router.patch('/atualizar/:id', autenticarAdmin, controllerAdocao.update);
 
 module.exports = router
